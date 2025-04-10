9c66bb79db4fcdd32c34ccff5e2a3986fe67a522
zainuamalik <zainul.abidin@dubizzlelabs.com>
2025-04-10 14:32:05 +0500

    added refresh token config

--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,7 @@ class OrderStatus(str, Enum):
 class Settings(BaseSettings):
     DATABASE_URL: str
     JWT_SECRET_KEY: str
+    JWT_REFRESH_SECRET_KEY: str
     JWT_ALGORITHM: str
     ACCESS_TOKEN_EXPIRE_MINUTES: int
     REFRESH_TOKEN_EXPIRE_MINUTES: int
